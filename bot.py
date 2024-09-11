953d3643041ec34611c560652abbb79213749e60
Nick088 <91847579+Nick088Official@users.noreply.github.com>
2024-09-11 10:27:57 +0200

    Update groq llm models

--- bot.py
+++ bot.py
@@ -91,11 +91,13 @@ authorized_roles = [1198707036070871102]   # Replace with role IDs
 groq_models = [
     "llama3-70b-8192",
     "llama3-8b-8192",
-    "gemma-7b-it",
     "mixtral-8x7b-32768",
     "llama-3.1-405b-reasoning",
     "llama-3.1-70b-versatile",
     "llama-3.1-8b-instant",
+    "gemma-7b-it",
+    "gemma2-9b-it",
+    "llava-v1.5-7b-4096-preview",
     "llama-guard-3-8b"
 ]
 
