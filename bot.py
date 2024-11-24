f2235bb4050fdd5c42b61ec60815647fdd1217e4
Nick088 <91847579+Nick088Official@users.noreply.github.com>
2024-11-25 00:17:18 +0100

    gemini models fix top k
    fix '400 Unable to submit request because it has a topK value of 64 but the supported range is from 1 (inclusive) to 41 (exclusive). Update the value and try again.' for gemini models, lowering the top k

--- bot.py
+++ bot.py
@@ -1619,7 +1619,7 @@ word_message_reactions = {
 generation_config = {
   "temperature": 1,
   "top_p": 0.95,
-  "top_k": 64,
+  "top_k": 40,
   "max_output_tokens": 8192,
   "response_mime_type": "text/plain",
 }
